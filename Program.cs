a58795311ce03b2ece97d281d52aefa21a3b2972
AhmedShaker940 <a7medsar7an123@gmail.com>
2026-08-28 05:59:09 +0300

    Question01

--- Program.cs
+++ Program.cs
@@ -4,6 +4,18 @@
     {
         static void Main(string[] args)
         {
+            #region Question01
+            //Q1: What is a generic class? Why use generics?
+            /*
+             
+            - Generic class is a type of classes that is marked with <> and allow us to use different type parameters which is specified in main 
+            - Generics has many benifits such as : 
+                      - Type Safety which allow us to find Errors at compile time (before the program runs)
+                      - Code Usability which allow us to write the code once then use it in many forms after we specify the type of the parameter
+                      - it allow us to Avoid boxing and unboxing (better performance not like Object)
+             
+             */ 
+            #endregion
 
         }
     }
